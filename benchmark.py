6941da1da1412a84983dc21dd09b507add3332de
Thomas Servotte <thomas.servotte@student.uantwerpen.be>
2024-10-18 15:06:32 +0000

    fix: processed ids

--- benchmark.py
+++ benchmark.py
@@ -8,7 +8,7 @@ from benchmark_config import UCI_DATASET_IDS
 from benchmark_util import *
 
 OUTPUTFOLDER = pathlib.Path(__file__).parent / "Output"
-experiment_file = OUTPUTFOLDER / "benchmark_results.csv"
+experiment_file = OUTPUTFOLDER / "benchmark_results_v2.csv"
 print(f"Results will be stored in {experiment_file}")
 np.random.seed(42)
 cv = KFold(n_splits=5, shuffle=True, random_state=42)
@@ -19,6 +19,7 @@ if experiment_file.exists():
     results = results.to_dict("records")
 else:
     results = []
+    processed_repo_ids = []
 
 repo_ids_to_process = [repo_id for repo_id in UCI_DATASET_IDS if repo_id not in processed_repo_ids]
 for repo_id in repo_ids_to_process:
